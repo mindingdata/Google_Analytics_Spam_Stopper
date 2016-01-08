cf57b5448bd501b635d8425b2e57a541aaf78d80
Wade <wade.gausden@gmail.com>
2016-01-08 15:58:56 +1300

    Fixed formatting with the output file.

--- Node/run.js
+++ Node/run.js
@@ -1,13 +1,12 @@
 var fs = require('fs');
 var array = fs.readFileSync('../spamlist.txt').toString().split("\r\n");
-var concatenated = '{';
+var concatenated = '';
 for(i in array) {
-    concatenated += array[i];
+    concatenated += array[i].replace('\.', '\\.');
     console.log('Loading : ' + array[i]);
     
     if(i < array.length - 1)
         concatenated += '|';
 }
-concatenated += '}';
 console.log('Completed output : ' + concatenated);
 fs.writeFile('../output.txt', concatenated);
